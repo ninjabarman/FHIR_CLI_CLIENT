b84754f2076e24e02b42f83ec984099efdf29846
Ben Arvan <bnarvan@gmail.com>
2016-04-05 15:40:16 -0500

    Diagnostic Order

--- FHIR_CLI_Cient/Program.cs
+++ FHIR_CLI_Cient/Program.cs
@@ -7,48 +7,30 @@ using System.Configuration;
 using Hl7.Fhir.Rest;
 using Hl7.Fhir.Model;
 using Hl7.Fhir.Serialization;
-using FHIR_CLI_Client.Services;
 using System.Web;
 using System.Net;
+using Newtonsoft.Json;
+
 namespace FHIR_CLI_Client
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Creates a string representing an instance of DiagnosticOrder which has been serialized to json
+        /// </summary>
+        /// <returns>string</returns>
+        private static string GenerateDiagnosticOrder(string subject, int i, int j)
         {
-            #region
-            /**
-            var client = new FhirClient(ConfigurationManager.AppSettings["SparkServerURI"]);
-
-            //set up the services
-            var patientService = new PatientService(client);
-            var allergyService = new AllergyService(client);
-            var observationService = new ObservationService(client);
-            var medicationService = new MedicationService(client);
-
-            //Part1
-
-            //Part 2: create three stubb objects
-            patientService.AddStubsToServer();
-
-            var stubs = patientService.GenerateStubExamples();
-            bool successful = true;
-            foreach(var stub in stubs)
-            {
-                var res = client.SearchById<Patient>(stub.Id).GetResources().Select(s => (Patient) s).ToList();
-                if (!res.Contains(stub))
-                    successful = false;
-            }
-            if (successful)
-                Console.WriteLine("Stub examples created successfully on server");
-            else
-                Console.WriteLine("ERROR: Something went wrong.");
-            **/
-            #endregion
             var order = new DiagnosticOrder();
             order.Subject = new ResourceReference();
-            order.Subject.Reference = "spark1934";
-            string content = FhirSerializer.SerializeResourceToJson(order);
+            order.Subject.Reference = subject;
+            string time = JsonConvert.SerializeObject(new Dictionary<string, int>() { { "start", i }, { "end", j } });
+            order.FhirComments = new List<string>() { time };
+            return FhirSerializer.SerializeResourceToJson(order);
+        }
+        static void Main(string[] args)
+        {
+            string content = GenerateDiagnosticOrder("spark1934", 1, 1);
             var bytes = System.Text.Encoding.UTF8.GetBytes(content);
             WebRequest request = WebRequest.Create("http://localhost:49438/api/v1/values");
             request.Method = "POST";
